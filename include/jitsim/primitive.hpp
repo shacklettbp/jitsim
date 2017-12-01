cf773bb9e06fc0496d19bdb37bc770c39929117e
Jennifer <jenntao@stanford.edu>
2017-12-01 00:52:35 -0800

    primitive constructor sans make_def

--- include/jitsim/primitive.hpp
+++ include/jitsim/primitive.hpp
@@ -9,11 +9,15 @@
 namespace JITSim {
 
 struct Primitive {
+private:
+  bool has_definition;
+
 public:
   bool is_stateful;
   std::function<llvm::Value *(FunctionEnvironment &env, const std::vector<llvm::Value *> &)> make_compute_output;
   std::function<void (FunctionEnvironment &env, const std::vector<llvm::Value *> &)> make_update_state;
   std::function<void (ModuleEnvironment &env)> make_def;
+
   Primitive(bool is_stateful_,
             std::function<llvm::Value *(FunctionEnvironment &, const std::vector<llvm::Value *> &)> make_compute_output_,
             std::function<void(FunctionEnvironment &, const std::vector<llvm::Value *> &)> make_update_state_,
@@ -22,7 +26,20 @@ public:
       make_compute_output(make_compute_output_),
       make_update_state(make_update_state_),
       make_def(make_def_)
-  {}
+  {
+    has_definition = true;
+  }
+  
+  Primitive(bool is_stateful_,
+            std::function<llvm::Value *(FunctionEnvironment &, const std::vector<llvm::Value *> &)> make_compute_output_,
+            std::function<void(FunctionEnvironment &, const std::vector<llvm::Value *> &)> make_update_state_)
+    : is_stateful(is_stateful_),
+      make_compute_output(make_compute_output_),
+      make_update_state(make_update_state_)
+  {
+    has_definition = false;
+  }
+
 };
 
 }
