eeaf024ed0b37c60c6f98cde0be71e95db665f0c
Jennifer <jenntao@stanford.edu>
2017-12-02 15:08:39 -0800

    circuit_llvm.cpp Value/Input renamed

--- src/circuit_llvm.cpp
+++ src/circuit_llvm.cpp
@@ -30,16 +30,16 @@ static ModuleEnvironment ModuleForPrimitive(Builder &builder,
 std::vector<Type *> getArgTypes(const Definition &definition, ModuleEnvironment &mod_env)
 {
   std::vector<Type *> arg_types;
-  for (const JITSim::Value &defn_input : definition.getIFace().getOutputs()) {
-    arg_types.push_back(Type::getIntNTy(mod_env.getContext(), defn_input.getWidth()));
+  for (const JITSim::Source &defn_sink : definition.getIFace().getSources()) {
+    arg_types.push_back(Type::getIntNTy(mod_env.getContext(), defn_sink.getWidth()));
   }
 
   return arg_types;
 }
 
-std::string getComputeOutputName(const Definition &definition)
+std::string getComputeSourceName(const Definition &definition)
 {
-  return Twine(definition.getSafeName(), "_compute_outputs").str();
+  return Twine(definition.getSafeName(), "_compute_sources").str();
 }
 
 std::string getUpdateStateName(const Definition &definition)
@@ -49,18 +49,18 @@ std::string getUpdateStateName(const Definition &definition)
 
 static StructType *makeReturnType(const Definition &definition, ModuleEnvironment &mod_env)
 {
-  std::string out_type_name = definition.getSafeName() + "_output_type";
+  std::string out_type_name = definition.getSafeName() + "_source_type";
   std::vector<Type *> elem_types;
-  for (const JITSim::Input &defn_output : definition.getIFace().getInputs()) {
-    elem_types.push_back(Type::getIntNTy(mod_env.getContext(), defn_output.getWidth()));
+  for (const JITSim::Sink &defn_source : definition.getIFace().getSinks()) {
+    elem_types.push_back(Type::getIntNTy(mod_env.getContext(), defn_source.getWidth()));
   }
 
   return StructType::create(mod_env.getContext(), elem_types, out_type_name);
 }
 
-static FunctionType * makeComputeOutputType(const Definition &definition, ModuleEnvironment &mod_env) 
+static FunctionType * makeComputeSourceType(const Definition &definition, ModuleEnvironment &mod_env) 
 {
-  Function *decl = mod_env.getFunctionDecl(getComputeOutputName(definition));
+  Function *decl = mod_env.getFunctionDecl(getComputeSourceName(definition));
   if (decl) {
     return decl->getFunctionType();
   }
@@ -109,23 +109,23 @@ static llvm::Value * createSlice(llvm::Value *whole, int offset, int width, Func
 static llvm::Value * makeValueReference(const Select &select, FunctionEnvironment &env)
 {
   if (select.isDirect()) {
-    const ValueSlice &slice = select.getDirect();
+    const SourceSlice &slice = select.getDirect();
     if (slice.isConstant()) {
       const APInt &const_int = slice.getConstant();
       return ConstantInt::get(env.getContext(), const_int);
     }
     else {
-      return env.lookupValue(slice.getValue());
+      return env.lookupSource(slice.getSource());
     }
   } else {
     llvm::Value *acc = nullptr;
     int total_width = 0;
-    for (const ValueSlice &slice : select.getSlices()) {
+    for (const SourceSlice &slice : select.getSlices()) {
       llvm::Value *sliced_val;
       if (slice.isConstant()) {
         sliced_val = ConstantInt::get(env.getContext(), slice.getConstant());
       } else {
-        llvm::Value *whole_val = env.lookupValue(slice.getValue());
+        llvm::Value *whole_val = env.lookupSource(slice.getSource());
         sliced_val = createSlice(whole_val, slice.getOffset(), slice.getWidth(), env);
       }
       total_width += slice.getWidth();
@@ -152,18 +152,18 @@ static llvm::Value * incrementStatePtr(llvm::Value *cur_ptr, int incr, FunctionE
   return env.getIRBuilder().CreateGEP(cur_ptr, ConstantInt::get(env.getContext(), APInt(64, incr, false)));
 }
 
-static void makeInstanceComputeOutput(const Instance *inst, const SimInfo &defn_info, FunctionEnvironment &env, llvm::Value *base_state)
+static void makeInstanceComputeSource(const Instance *inst, const SimInfo &defn_info, FunctionEnvironment &env, llvm::Value *base_state)
 {
   const SimInfo &info = inst->getDefinition().getSimInfo();
   const IFace &iface = inst->getIFace();
-  const std::vector<JITSim::Value> &outputs = iface.getOutputs();
+  const std::vector<JITSim::Source> &sources = iface.getSources();
 
   std::vector<llvm::Value *> argument_values;
 
   if (!info.isStateful()) {
-    /* FIXME do this input by input to support more circuits */
-    for (const Input &input : iface.getInputs()) {
-      argument_values.push_back(makeValueReference(input.getSelect(), env));
+    /* FIXME do this sink by sink to support more circuits */
+    for (const Sink &sink : iface.getSinks()) {
+      argument_values.push_back(makeValueReference(sink.getSelect(), env));
     }
   }
 
@@ -175,23 +175,23 @@ static void makeInstanceComputeOutput(const Instance *inst, const SimInfo &defn_
   std::vector<llvm::Value *> ret_values;
   if (info.isPrimitive()) {
     const Primitive &prim = info.getPrimitive();
-    ret_values = prim.make_compute_output(env, argument_values, *inst);
+    ret_values = prim.make_compute_source(env, argument_values, *inst);
   } else {
-    std::string inst_comp_outputs = getComputeOutputName(inst->getDefinition());
-    Function *inst_func = env.getModule().getFunctionDecl(inst_comp_outputs);
+    std::string inst_comp_sources = getComputeSourceName(inst->getDefinition());
+    Function *inst_func = env.getModule().getFunctionDecl(inst_comp_sources);
     if (inst_func == nullptr) {
-      inst_func = env.getModule().makeFunctionDecl(inst_comp_outputs, makeComputeOutputType(inst->getDefinition(), env.getModule()));
+      inst_func = env.getModule().makeFunctionDecl(inst_comp_sources, makeComputeSourceType(inst->getDefinition(), env.getModule()));
     }
 
-    llvm::Value *ret_struct = env.getIRBuilder().CreateCall(inst_func, argument_values, "inst_output");
-    for (unsigned i = 0; i < outputs.size(); i++) {
+    llvm::Value *ret_struct = env.getIRBuilder().CreateCall(inst_func, argument_values, "inst_source");
+    for (unsigned i = 0; i < sources.size(); i++) {
       llvm::Value *struct_elem = env.getIRBuilder().CreateExtractValue(ret_struct, { i });
       ret_values.push_back(struct_elem);
     }
   }
 
   for (unsigned i = 0; i < ret_values.size(); i++) {
-    env.addValue(&outputs[i], ret_values[i]);
+    env.addSource(&sources[i], ret_values[i]);
   }
 }
 
@@ -202,8 +202,8 @@ static void makeInstanceUpdateState(const Instance *inst, const SimInfo &defn_in
 
   std::vector<llvm::Value *> argument_values;
 
-  for (const Input &input : iface.getInputs()) {
-    argument_values.push_back(makeValueReference(input.getSelect(), env));
+  for (const Sink &sink : iface.getSinks()) {
+    argument_values.push_back(makeValueReference(sink.getSelect(), env));
   }
 
   llvm::Value *state_ptr = incrementStatePtr(base_state, defn_info.getOffset(inst), env);
@@ -224,48 +224,48 @@ static void makeInstanceUpdateState(const Instance *inst, const SimInfo &defn_in
   }
 }
 
-static void makeComputeOutputDefn(const Definition &definition, ModuleEnvironment &mod_env)
+static void makeComputeSourceDefn(const Definition &definition, ModuleEnvironment &mod_env)
 {
   const SimInfo &defn_info = definition.getSimInfo();
   bool is_stateful = defn_info.isStateful();
 
-  FunctionType *co_type = makeComputeOutputType(definition, mod_env);
-  FunctionEnvironment compute_outputs = mod_env.makeFunction(getComputeOutputName(definition), co_type);
-  compute_outputs.addBasicBlock("entry");
+  FunctionType *co_type = makeComputeSourceType(definition, mod_env);
+  FunctionEnvironment compute_sources = mod_env.makeFunction(getComputeSourceName(definition), co_type);
+  compute_sources.addBasicBlock("entry");
 
-  const std::vector<JITSim::Value> & outputs = definition.getIFace().getOutputs();
-  auto arg = compute_outputs.getFunction()->arg_begin();
+  const std::vector<JITSim::Source> & sources = definition.getIFace().getSources();
+  auto arg = compute_sources.getFunction()->arg_begin();
   if (!is_stateful) {
-    for (unsigned i = 0; i < outputs.size(); i++, arg++) {
-      const JITSim::Value *val = &outputs[i];
+    for (unsigned i = 0; i < sources.size(); i++, arg++) {
+      const JITSim::Source *src = &sources[i];
 
-      compute_outputs.addValue(val, arg);
-      arg->setName("self." + val->getName());
+      compute_sources.addSource(src, arg);
+      arg->setName("self." + src->getName());
     }
   }
 
   llvm::Value *state_ptr = nullptr;
   if (is_stateful) {
-    state_ptr = compute_outputs.getFunction()->arg_end() - 1;
+    state_ptr = compute_sources.getFunction()->arg_end() - 1;
     state_ptr->setName("state_ptr");
   }
 
-  const std::vector<const Instance *> &output_deps = defn_info.getOutputDeps();
-  for (const Instance *inst : output_deps) {
-    makeInstanceComputeOutput(inst, defn_info, compute_outputs, state_ptr);
+  const std::vector<const Instance *> &source_deps = defn_info.getSourceDeps();
+  for (const Instance *inst : source_deps) {
+    makeInstanceComputeSource(inst, defn_info, compute_sources, state_ptr);
   }
 
-  const std::vector<JITSim::Input> & inputs = definition.getIFace().getInputs();
+  const std::vector<JITSim::Sink> & sinks = definition.getIFace().getSinks();
   llvm::Value *ret_val = UndefValue::get(co_type->getReturnType());
 
-  for (unsigned i = 0; i < inputs.size(); i++ ) {
-    llvm::Value *ret_part = makeValueReference(inputs[i].getSelect(), compute_outputs);
-    ret_val = compute_outputs.getIRBuilder().CreateInsertValue(ret_val, ret_part, { i });
+  for (unsigned i = 0; i < sinks.size(); i++ ) {
+    llvm::Value *ret_part = makeValueReference(sinks[i].getSelect(), compute_sources);
+    ret_val = compute_sources.getIRBuilder().CreateInsertValue(ret_val, ret_part, { i });
   }
 
-  compute_outputs.getIRBuilder().CreateRet(ret_val);
+  compute_sources.getIRBuilder().CreateRet(ret_val);
 
-  compute_outputs.verify();
+  compute_sources.verify();
 }
 
 static void makeUpdateStateDefn(const Definition &definition, ModuleEnvironment &mod_env)
@@ -276,21 +276,21 @@ static void makeUpdateStateDefn(const Definition &definition, ModuleEnvironment 
   FunctionEnvironment update_state = mod_env.makeFunction(getUpdateStateName(definition), us_type);
   update_state.addBasicBlock("entry");
 
-  const std::vector<JITSim::Value> & outputs = definition.getIFace().getOutputs();
+  const std::vector<JITSim::Source> & sources = definition.getIFace().getSources();
   auto arg = update_state.getFunction()->arg_begin();
 
-  for (unsigned i = 0; i < outputs.size(); i++, arg++) {
-    const JITSim::Value *val = &outputs[i];
+  for (unsigned i = 0; i < sources.size(); i++, arg++) {
+    const JITSim::Source *src = &sources[i];
 
-    update_state.addValue(val, arg);
-    arg->setName("self." + val->getName());
+    update_state.addSource(src, arg);
+    arg->setName("self." + src->getName());
   }
 
   llvm::Value *state_ptr = update_state.getFunction()->arg_end() - 1;
   state_ptr->setName("state_ptr");
 
   for (const Instance *inst : defn_info.getStateDeps()) {
-    makeInstanceComputeOutput(inst, defn_info, update_state, state_ptr);
+    makeInstanceComputeSource(inst, defn_info, update_state, state_ptr);
   }
 
   for (const Instance *inst : defn_info.getStatefulInstances()) {
@@ -310,7 +310,7 @@ ModuleEnvironment ModuleForDefinition(Builder &builder, const Definition &defini
 
   ModuleEnvironment mod_env = builder.makeModule(definition.getSafeName());
 
-  makeComputeOutputDefn(definition, mod_env);
+  makeComputeSourceDefn(definition, mod_env);
 
   if (siminfo.isStateful()) {
     makeUpdateStateDefn(definition, mod_env);
