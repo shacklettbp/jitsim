d11eb78ebe84c3efcaba075c9dd03f01d3b6d739
Brennan Shacklett <bpshacklett@gmail.com>
2017-12-03 17:30:41 -0800

    super_simple.json works now

--- binsrc/main.cpp
+++ binsrc/main.cpp
@@ -67,14 +67,14 @@ int main(int argc, char *argv[])
   /* ------- Execute here ------- */
 
   const Definition &top = circuit.getTopDefinition();
-  void (*update_state)(uint8_t *) = (void (*)(uint8_t *))jit.getSymbolAddress(top.getSafeName() + "_update_state");
-  int (*compute_output)(uint8_t *) = (int (*)(uint8_t *))jit.getSymbolAddress(top.getSafeName() + "_compute_output");
+  auto update_state = (void (*)(unsigned , uint8_t *))jit.getSymbolAddress(top.getSafeName() + "_update_state");
+  auto compute_output = (int (*)(unsigned, uint8_t *))jit.getSymbolAddress(top.getSafeName() + "_compute_output");
   assert(update_state && compute_output);
 
   vector<uint8_t> state = top.getSimInfo().allocateState();
   for (unsigned i = 0; i < 30; i++) {
-    int x = compute_output(state.data());
-    update_state(state.data());
+    int x = compute_output(i, state.data());
+    update_state(i, state.data());
     cout << x << endl;
   }
 
