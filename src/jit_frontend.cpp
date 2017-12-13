140ef5b69a7b03959076f8c81b9aced59f074e98
Brennan Shacklett <bpshacklett@gmail.com>
2017-12-13 12:24:57 -0800

    Crappy get values

--- src/jit_frontend.cpp
+++ src/jit_frontend.cpp
@@ -2,6 +2,8 @@
 #include "utils.hpp"
 #include "llvm_utils.hpp"
 
+#include <llvm/IR/ValueSymbolTable.h>
+
 namespace JITSim {
 
 using namespace std;
@@ -103,13 +105,60 @@ void JITFrontend::addDefinitionFunctions(const Definition &defn)
 
   jit.addLazyFunction(defn.getSafeName() + "_state_deps", [this, &defn]() {
     return MakeStateDeps(builder, defn);
-  }, [this](auto module) {
+  }, [this, &defn](auto module) {
     return module;
   });
 
   jit.addLazyFunction(defn.getSafeName() + "_output_deps", [this, &defn]() {
     return MakeOutputDeps(builder, defn);
-  }, [this](auto module) {
+  }, [this, &defn](auto module) {
+    auto iter = debug_info.debug_values.find(&defn);
+    if (iter == debug_info.debug_values.end()) {
+      return module;
+    } 
+
+    auto func = module->getFunction(defn.getSafeName() + "_output_deps");
+    assert(func && "unable to find function to modify");
+    auto symtab = func->getValueSymbolTable();
+    assert(symtab && "unable to find function symbol table");
+
+    llvm::Value *inst_offset = symtab->lookup("inst_offset");
+    assert(inst_offset && "Can't find inst_offset param");
+    llvm::BasicBlock &first_bb = func->front();
+    llvm::IRBuilder<> ir_builder(builder.getContext());
+
+    const vector<DebugValue> &inspect_vals = iter->second;
+    for (const DebugValue &val : inspect_vals) {
+      llvm::BasicBlock *successor = first_bb.getSingleSuccessor();
+      llvm::BasicBlock *debug_block = llvm::BasicBlock::Create(builder.getContext(), "debug_block", func);
+      llvm::Instruction *term = first_bb.getTerminator();
+
+      ir_builder.SetInsertPoint(term);
+      ir_builder.CreateBr(debug_block);
+      ir_builder.SetInsertPoint(debug_block);
+
+      llvm::Value *inst_eq = ir_builder.CreateICmpEQ(inst_offset, llvm::ConstantInt::get(builder.getContext(), llvm::APInt(64, val.inst_num)));
+      llvm::BasicBlock *val_save = llvm::BasicBlock::Create(builder.getContext(), "inst_match", func);
+      ir_builder.CreateCondBr(inst_eq, val_save, successor);
+      ir_builder.SetInsertPoint(val_save);
+
+      llvm::Value *llvm_val = symtab->lookup(val.input_name);
+      assert(llvm_val && "Can't find queried value");
+      assert(val.store.data() && "Data store is null?");
+
+      llvm::Value *addr = llvm::Constant::getIntegerValue(llvm_val->getType()->getPointerTo(), llvm::APInt(64, (uint64_t)val.store.data()));
+      ir_builder.CreateStore(llvm_val, addr);
+      ir_builder.CreateBr(successor);
+
+      term->eraseFromParent();
+    }
+
+    llvm::outs() << "========== Modified " << defn.getName() << " ===========\n";
+    llvm::outs() << *module;
+    llvm::outs() << "=====================================\n";
+    if (llvm::verifyModule(*module)) {
+      llvm::errs() << "Modified module has errors\n";
+    }
     return module;
   });
 }
@@ -129,18 +178,20 @@ void JITFrontend::addWrappers(const Definition &top)
   });
 }
 
-JITFrontend::JITFrontend(const Circuit &circuit, const Definition &top)
+JITFrontend::JITFrontend(const Circuit &circuit, const Definition &top_)
   : target_machine(llvm::EngineBuilder().selectTarget()),
     data_layout(target_machine->createDataLayout()),
     builder(data_layout, *target_machine),
     jit(*target_machine, data_layout),
-    co_in(top.getSimInfo().getOutputSources(), data_layout, builder.getContext()),
-    co_out(top.getIFace().getSinks(), data_layout, builder.getContext()),
-    us_in(top.getSimInfo().getStateSources(), data_layout, builder.getContext()),
-    gv_in(top.getIFace().getSources(), data_layout, builder.getContext()),
-    state(top.getSimInfo().allocateState()),
+    co_in(top_.getSimInfo().getOutputSources(), data_layout, builder.getContext()),
+    co_out(top_.getIFace().getSinks(), data_layout, builder.getContext()),
+    us_in(top_.getSimInfo().getStateSources(), data_layout, builder.getContext()),
+    gv_in(top_.getIFace().getSources(), data_layout, builder.getContext()),
+    state(top_.getSimInfo().allocateState()),
     compute_output_ptr(nullptr),
-    update_state_ptr(nullptr)
+    update_state_ptr(nullptr),
+    debug_info(),
+    top(&top_)
 {
   for (const Definition &defn : circuit.getDefinitions()) {
     if (!isPrimitive(defn)) {
@@ -149,7 +200,7 @@ JITFrontend::JITFrontend(const Circuit &circuit, const Definition &top)
       // FIXME handle primitives that want to provide function definitions
     }
   }
-  addWrappers(top);
+  addWrappers(top_);
 
   compute_output_ptr = (WrapperComputeOutputFn)jit.getSymbolAddress("compute_output");
   update_state_ptr = (WrapperUpdateStateFn)jit.getSymbolAddress("update_state");
@@ -185,12 +236,70 @@ const LLVMStruct & JITFrontend::computeOutput()
   return co_out;
 }
 
+static tuple<const Definition *, const Instance *, unsigned> getDefnAndInst(const Definition *top, const vector<string> &inst_names)
+{
+  const Definition *cur_defn = top;
+  unsigned inst_num = 0;
+  for (unsigned i = 0; i < inst_names.size() - 1; i++) {
+    const string &inst_name = inst_names[i];
+    const SimInfo &sim_info = cur_defn->getSimInfo();
+    const Instance &inst = cur_defn->getInstance(inst_name);
+    cur_defn = &inst.getDefinition();
+    inst_num += sim_info.getInstNum(&inst);
+  }
+
+  const Instance *inst = &cur_defn->getInstance(inst_names.back());
+
+  return make_tuple(cur_defn, inst, inst_num);
+}
+
+vector<uint8_t> & JITFrontend::updateDebugInfo(const vector<string> &inst_names, const string &input)
+{
+  const Definition *defn;
+  const Instance *inst;
+  unsigned inst_num;
+  tie(defn, inst, inst_num) = getDefnAndInst(top, inst_names);
+
+  int num_bits = 0;
+  const IFace &iface = inst->getIFace();
+  if (iface.hasSource(input)) {
+    const Source *src = iface.getSource(input);
+    num_bits = src->getWidth();
+  } else if (iface.hasSink(input)) {
+    const Sink *sink = iface.getSink(input);
+    num_bits = sink->getWidth();
+  }
+
+  unsigned num_bytes = num_bits / 8;
+  if (num_bits % 8 != 0) {
+    num_bytes++;
+  }
+  
+  assert(num_bytes);
+  vector<uint8_t> store(num_bytes, 0);
+
+  string input_name = inst->getName() + "_" + input;
+
+  debug_info.debug_values[defn].push_back(DebugValue {inst_num, input_name, move(store)});
+  assert(debug_info.debug_values[defn].back().store.data());
+
+  return debug_info.debug_values[defn].back().store;
+}
+
 llvm::APInt JITFrontend::getValue(const vector<string> &inst_names, const string &input)
 {
   jit.purgeDebugModules();
-  get_values_ptr(gv_in.getData(), state.data(), nullptr);
+  vector<uint8_t> &data = updateDebugInfo(inst_names, input);
+  get_values_ptr(gv_in.getData(), state.data());
 
-  return llvm::APInt(64, 0);
+  int num64s = data.size() / 8;
+  if (data.size() % 8 != 0) {
+    num64s++;
+  }
+  vector<uint64_t> safe_arr(num64s, 0);
+  memcpy(safe_arr.data(), data.data(), data.size());
+
+  return llvm::APInt(data.size()*8, llvm::ArrayRef<uint64_t>(safe_arr.data(), num64s));
 }
 
 void JITFrontend::dumpIR()
